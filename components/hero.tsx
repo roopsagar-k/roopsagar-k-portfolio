ff35698aa328ccf2b27bcaac89fba09cac9010e2
roopsagar-k <roopsagarudayar9854@gmail.com>
2024-08-31 14:39:26 +0530

    avatar update

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "./ui/spotlight";
 import TechStack from "./TechStack";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Hero() {
   return (
@@ -18,12 +20,17 @@ export default function Hero() {
           Hi! I&apos;m Roopsagar K, a Full Stack Web Developer from India.
         </p>
         <center className="relative">
-            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] mt-4 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <div className="flex gap-3 items-center justify-center">
+            <Link href={"#projects"} className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] mt-4 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-2 text-sm font-medium text-white backdrop-blur-3xl">
                     Show my work
                 </span>
-            </button>
+            </Link>
+            <a href="./1HK21CS129_Roopsagar_K.pdf" download="Roopsagar_K_Resume.pdf">
+              <Button className="rounded-full mt-4 py-2">Download CV</Button>
+            </a>
+          </div>
             <div className="absolute mt-40 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <TechStack />
             </div>
